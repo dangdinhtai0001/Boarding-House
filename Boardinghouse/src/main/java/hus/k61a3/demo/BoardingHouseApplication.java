d831cb74416f7723cb66d3b4fab8d59ff6d93a6c
dinhtai <0dangdinhtai0001>
2018-10-27 14:28:24 +0700

    create package

---
+++ Boardinghouse/src/main/java/hus/k61a3/demo/BoardingHouseApplication.java
@@ -0,0 +1,12 @@
+package hus.k61a3.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardingHouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BoardingHouseApplication.class, args);
+    }
+}
---
+++ Boardinghouse/src/test/java/hus/k61a3/demo/BoardingHouseApplicationTests.java
@@ -0,0 +1,16 @@
+package hus.k61a3.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BoardingHouseApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
