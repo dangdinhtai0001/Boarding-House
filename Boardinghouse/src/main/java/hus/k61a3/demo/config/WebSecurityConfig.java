1ade226d7dbad9f7f86eed81ee081af019b6c3fe
dinhtai <0dangdinhtai0001>
2018-10-30 15:47:18 +0700

    xong remember me

--- Boardinghouse/src/main/java/hus/k61a3/demo/config/WebSecurityConfig.java
+++ Boardinghouse/src/main/java/hus/k61a3/demo/config/WebSecurityConfig.java
@@ -16,43 +16,6 @@ import org.springframework.transaction.annotation.Transactional;
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-//    @Bean
-//    public BCryptPasswordEncoder passwordEncoder() {
-//        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
-//        return bCryptPasswordEncoder;
-//    }
-//
-//    @Autowired
-//    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.inMemoryAuthentication().passwordEncoder(passwordEncoder()).
-//                withUser("kai").password("$2a$04$Q2Cq0k57zf2Vs/n3JXwzmerql9RzElr.J7aQd3/Sq0fw/BdDFPAj.").roles("ADMIN");
-//        auth.inMemoryAuthentication().passwordEncoder(passwordEncoder()).
-//                withUser("sena").password("$2a$04$Q2Cq0k57zf2Vs/n3JXwzmerql9RzElr.J7aQd3/Sq0fw/BdDFPAj.").roles("USER");
-//
-////    auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance()).withUser("sena").password("123456").roles("USER");
-//    }
-//
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        // Chỉ cho phép user có quyền ADMIN truy cập đường dẫn /admin/**
-//        http.authorizeRequests().antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')");
-//        // Chỉ cho phép user có quyền ADMIN hoặc USER truy cập đường dẫn /user/**
-//        http.authorizeRequests().antMatchers("/user/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_USER')");
-//        // Khi người dùng đã login, với vai trò USER, Nhưng truy cập vào trang yêu cầu vai trò ADMIN, sẽ chuyển hướng tới trang /403
-//        http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
-//        // Cấu hình cho Login Form.
-//        http.authorizeRequests().and().formLogin()//
-//                .loginProcessingUrl("/j_spring_security_login")//
-//                .loginPage("/login")//
-//                .defaultSuccessUrl("/user")//
-//                .failureUrl("/login?message=error")//
-//                .usernameParameter("username")//
-//                .passwordParameter("password")
-//                // Cấu hình cho Logout Page.
-//                .and().logout().logoutUrl("/j_spring_security_logout").logoutSuccessUrl("/login?message=logout");
-//    }
-
-
     @Autowired
     private UserDetailsService userDetailsService;
 
@@ -78,10 +41,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         // Chỉ cho phép user có quyền ADMIN hoặc USER truy cập đường dẫn /user/**
 //        http.authorizeRequests().antMatchers("/user/**").access("hasRole('ADMIN') or hasRole('MEMBER')");
-        http.authorizeRequests().antMatchers("/user/**").hasAnyAuthority("ADMIN","MEMBER");
+        http.authorizeRequests().antMatchers("/user/**").hasAnyAuthority("ADMIN", "MEMBER");
 
         // Khi người dùng đã login, với vai trò USER, Nhưng truy cập vào trang yêu cầu vai trò ADMIN, sẽ chuyển hướng tới trang /403
         http.authorizeRequests().and().exceptionHandling().accessDeniedPage("/403");
+
+        //  Cấu hình remember me, thời gian là 1296000 giây
+        http.rememberMe().key("uniqueAndSecret").tokenValiditySeconds(1296000);
 
         // Cấu hình cho Login Form.
         http.authorizeRequests().and().formLogin()//
