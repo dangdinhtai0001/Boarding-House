fdfc5e4cc651f10192aebe20236ff2065c0e241c
dinhtai <0dangdinhtai0001>
2018-10-30 15:06:30 +0700

    Xong login dạng xấu 2

--- Boardinghouse/src/main/java/hus/k61a3/demo/controllers/Controller.java
+++ Boardinghouse/src/main/java/hus/k61a3/demo/controllers/Controller.java
@@ -3,7 +3,6 @@ package hus.k61a3.demo.controllers;
 import hus.k61a3.demo.services.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @org.springframework.stereotype.Controller
@@ -16,7 +15,7 @@ public class Controller {
 
     @RequestMapping(value = {"/", "/login"})
     public String login() {
-        return "login";
+        return "login2";
     }
 
     @RequestMapping("/user")
