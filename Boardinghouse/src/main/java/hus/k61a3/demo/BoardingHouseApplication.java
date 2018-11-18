716d19d4c6271b495ec2ad8f27debcd3fbe60d5c
dinhtai <0dangdinhtai0001>
2018-11-18 19:47:52 +0700

    test deploy heroku lần 17

--- Boardinghouse/src/main/java/hus/k61a3/demo/BoardingHouseApplication.java
+++ Boardinghouse/src/main/java/hus/k61a3/demo/BoardingHouseApplication.java
@@ -2,9 +2,15 @@ package hus.k61a3.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BoardingHouseApplication {
+public class BoardingHouseApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(BoardingHouseApplication .class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BoardingHouseApplication.class, args);
