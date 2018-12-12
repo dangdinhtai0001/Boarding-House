53e73bd9786af6bbb35a34ccaa86c216c269a985
hant98 <nguyenha98nq@gmail.com>
2018-12-12 23:23:08 +0700

    commit lai about us

--- Boardinghouse/src/main/java/hus/k61a3/demo/controllers/Controller.java
+++ Boardinghouse/src/main/java/hus/k61a3/demo/controllers/Controller.java
@@ -1,6 +1,7 @@
 package hus.k61a3.demo.controllers;
 
 import hus.k61a3.demo.Listings.ListingsService;
+import hus.k61a3.demo.aboutus.AboutusService;
 import hus.k61a3.demo.blog.entities.SubmitCommentForm;
 import hus.k61a3.demo.blog.services.BlogService;
 import hus.k61a3.demo.contact.ContactService;
@@ -38,6 +39,9 @@ public class Controller {
 
     @Autowired
     private ContactService contactService;
+
+    @Autowired
+    private AboutusService aboutusService;
 
 //    @Autowired
 //    private RoomService roomService;
@@ -123,5 +127,11 @@ public class Controller {
     public String game2048(Model model) {
         homeService.displayHome(model);
         return "game2048";
+    }
+
+    @RequestMapping(value = "/aboutus")
+    public String aboutUs(Model model){
+        aboutusService.displayAboutus(model);
+        return "aboutUs";
     }
 }
