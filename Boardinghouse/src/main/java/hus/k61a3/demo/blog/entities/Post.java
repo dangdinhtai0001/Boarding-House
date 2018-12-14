d117f5be3103967da1fdcbdc10c6bcb3a0d7abf4
dinhtai <0dangdinhtai0001>
2018-12-14 11:05:45 +0700

    Dùng thymeleaf để substring cho tính năng hiện ngắn gọn màn blog

--- Boardinghouse/src/main/java/hus/k61a3/demo/blog/entities/Post.java
+++ Boardinghouse/src/main/java/hus/k61a3/demo/blog/entities/Post.java
@@ -1,7 +1,6 @@
 package hus.k61a3.demo.blog.entities;
 
 import hus.k61a3.demo.blog.domains.Comment;
-import hus.k61a3.demo.blog.domains.Topic;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,14 +13,14 @@ import java.util.List;
 @NoArgsConstructor
 @ToString
 public class Post {
-    private String summary;
+//    private String summary;
     private hus.k61a3.demo.blog.domains.Post post;
     private List<Comment> comments;
     private int numberOfComment;
     public Post(hus.k61a3.demo.blog.domains.Post post, List<Comment> comments  ) {
         this.post = post;
         this.comments = comments;
-        this.summary = post.getContent().substring(0,200);
+//        this.summary = post.getContent().substring(0,200);
         this.numberOfComment = comments.size();
     }
 }
